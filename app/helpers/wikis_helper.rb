31e8aa5bb8be99c35f7862157f333e0c4f59042a
Gahee Heo <ghbooth12@gmail.com>
2016-03-23 16:14:25 -0500

    ch9-markdown: completed

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,4 +2,25 @@ module WikisHelper
   def user_is_authorized?(wiki)
     current_user && (current_user == wiki.user || current_user.admin?)
   end
+
+  def markdown_wiki(text)
+    options = {
+      filter_html:         true,
+      hard_wrap:           true,
+      link_attributes:     { rel: "nofollow", target: "_blank" }
+    }
+
+    extensions = {
+      space_after_headers:          true,
+      autolink:                     true,
+      superscript:                  true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks:           true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
