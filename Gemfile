5441dcf4bc6660b679d02e1e3a910e94bc1dba53
Gahee Heo <ghbooth12@gmail.com>
2016-03-22 22:17:25 -0500

    ch7-upgrading-an-account: completed

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'figaro'
 gem 'pry'
 gem 'devise'
 gem 'pundit'
+gem 'stripe'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :charges, only: [:new, :create]
+  delete 'charges/cancel_premium' => 'charges#cancel_premium', as: :cancel_premium
+
   resources :wikis
   devise_for :users
   root 'welcome#index'
