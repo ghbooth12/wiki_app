406ad7f53eead0b30c5a36dc451dd1121d0d6a68
Gahee Heo <ghbooth12@gmail.com>
2016-03-23 12:57:20 -0500

    ch8-private-wikis: completed

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,4 +2,15 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   scope :ordered_by_created_at, -> { order('created_at DESC') }
+  scope :visible_to, -> (user) do
+    if user.present? && (user.admin? || user.premium?)
+      all
+    else
+      where(private: false)
+    end
+  end
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true
 end
